80df25c5fd4da3c5144c1b3ba77153aad3c04d9f
Artur Gogiyan <artur.gogiyan@devexpress.com>
2019-12-14 16:20:39 +0300

    menu beta implementation

--- Src/main.c
+++ Src/main.c
@@ -97,278 +97,7 @@ void SystemClock_Config(void);
  * @brief  The application entry point.
  * @retval int
  */
-void transmit(char buf[], uint8_t buffSize) {
-	char bufWithEndOfLine[buffSize + 1];
-	for (int i = 0; i < buffSize; i++) {
-		bufWithEndOfLine[i] = buf[i];
-	}
-	bufWithEndOfLine[buffSize] = 13; // enter
-	if (isInterruptOn == 1) {
-		HAL_UART_Transmit_IT(&huart6, bufWithEndOfLine, buffSize + 1);
-	} else {
-		HAL_UART_Transmit(&huart6, bufWithEndOfLine, buffSize + 1,
-				TRANSMIT_DELAY);
-	}
-}
-
-void errorOutput(uint8_t num) {
-	char str[] = "[ 0]Wrong command";
-	if (num < 10) {
-		str[2] = '0' + num;
-	} else {
-		str[2] = '0' + num / 10;
-		str[1] = '0' + num % 10;
-	}
-
-	transmit(str, 17);
-}
-
-void lightsLogic() {
-	int newBtn = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15);
-	if (btn == 0 && newBtn == 1) {
-		currMode = (currMode + 1) % modesTotal;
-	}
-	btn = newBtn;
-
-	int fase = (timer / freqMultiplier[currMode]) % seqLengths[currMode];
-	if (timer % freqMultiplier[currMode] == 0) {
-		// change lights state
-		switch (currLightState) {
-		case 'r':
-			switch (modes[currMode][fase]) {
-			case 'g':
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
-				break;
-			case 'r':
-				break;
-			case 'y':
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
-				break;
-			case 'n':
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);
-				break;
-			}
-			break;
-		case 'g':
-			switch (modes[currMode][fase]) {
-			case 'g':
-				break;
-			case 'r':
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);
-				break;
-			case 'y':
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
-				break;
-			case 'n':
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
-				break;
-			}
-			break;
-		case 'y':
-			switch (modes[currMode][fase]) {
-			case 'g':
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
-				break;
-			case 'r':
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);
-				break;
-			case 'y':
-				break;
-			case 'n':
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
-				break;
-			}
-			break;
-		case 'n':
-			switch (modes[currMode][fase]) {
-			case 'g':
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
-				break;
-			case 'r':
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);
-				break;
-			case 'y':
-				HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
-				break;
-			case 'n':
-				break;
-			}
-			break;
-		}
-		currLightState = modes[currMode][fase];
-	}
-	timer += 1;
-}
-
-void proceedInput() {
-	if (rxByte[0] != 0x0D) {
-		if (inputStrSize < 18) {
-			if (isInterruptOn == 1
-					|| (isInterruptOn == 0 && byteReceiveStatus == HAL_OK)) {
-				inputStr[inputStrSize] = rxByte[0];
-				inputStrSize++;
-				transmit("                 ", 18);
-				transmit(inputStr, inputStrSize);
-			}
-		}
-	} else {
-		char strSet[] = "set ";
-		char strNew[] = "new ";
-		char strSetInterruptsOn[] = "set interrupts on";
-		char strSetInterruptsOff[] = "set interrupts off";
-
-		// set
-		if (strncmp(inputStr, strSet, 4) == 0 && inputStrSize == 5) {
-			uint8_t mode = inputStr[4] - '0';
-			if (mode < 0 || mode > 9 || mode >= modesTotal) {
-				errorOutput(1);
-				goto endOfCommandChecking;
-			}
-
-			currMode = mode;
-
-			transmit(inputStr, 5);
-
-			// new
-		} else if (strncmp(inputStr, strNew, 4) == 0 && inputStrSize > 4) {
-			int i = 4;
-			while (i < inputStrSize && i < 12) {
-				if (inputStr[i] == 'g' || inputStr[i] == 'r'
-						|| inputStr[i] == 'y' || inputStr[i] == 'n') {
-					modes[modesTotal][i - 4] = inputStr[i];
-				} else {
-					errorOutput(2);
-					goto endOfCommandChecking;
-				}
-				i++;
-			}
-			seqLengths[modesTotal] = i - 4;
-			isFreqInput = 1;
-
-			transmit("Input frequency multiplier from 1 to 4", 38);
-
-			// input frequency for 'new' command
-		} else if (isFreqInput == 1) {
-			isFreqInput = 0;
-			if (inputStrSize != 1) {
-				errorOutput(3);
-				goto endOfCommandChecking;
-			}
-			uint8_t freq = inputStr[0] - '0';
-			if (freq < 1 || freq > 4) {
-				errorOutput(4);
-				goto endOfCommandChecking;
-			}
-			freqMultiplier[modesTotal] = 5 - freq;
-			modesTotal++;
-
-			transmit(inputStr, inputStrSize);
-
-			// set interrupts on
-		} else if (strncmp(inputStr, strSetInterruptsOn, 17) == 0) {
-			if (inputStrSize != 17) {
-				errorOutput(5);
-				goto endOfCommandChecking;
-			}
-			if (isInterruptOn != 0) {
-				errorOutput(6);
-				goto endOfCommandChecking;
-			}
-			isInterruptOn = 1;
-			transmit("set interrupts on", 17);
-
-			// set interrupts off
-		} else if (strncmp(inputStr, strSetInterruptsOff, 18) == 0) {
-			if (inputStrSize != 18) {
-				errorOutput(7);
-				goto endOfCommandChecking;
-			}
-			if (isInterruptOn != 1) {
-				errorOutput(8);
-				goto endOfCommandChecking;
-			}
-			isInterruptOn = 0;
-			transmit("set interrupts off", 18);
-
-			// unknown command
-		} else {
-			transmit(inputStr, inputStrSize);
-			errorOutput(9);
-		}
-
-		endOfCommandChecking: inputStrSize = 0;
-	}
-
-	if (isInterruptOn == 1) {
-		HAL_UART_Receive_IT(&huart6, rxByte, 1);
-	} else {
-		rxByte[0] = '+';
-		byteReceiveStatus = -1;
-	}
-}
-
-//void receive(char buf[], uint8_t buffSize) {
-//	if (isInterruptOn == 1) {
-//		HAL_UART_Receive_IT(&huart6, buf, buffSize);
-//	} else {
-//		HAL_UART_Receive(&huart6, buf, buffSize, RECEIVE_DELAY);
-//	}
-//}
-
-print_menu(int position) {
-	char new_game[50];
-	char load[50];
-	char save[50];
-	char settings[50];
-	oled_Fill(Black);
-
-	oled_Fill(Black);
-	sprintf(new_game, "%cNew Game", position == 1 ? '>' : ' ');
-	sprintf(load, "%cLoad", position == 2 ? '>' : ' ');
-	sprintf(save, "%cSave", position == 3 ? '>' : ' ');
-	sprintf(settings, "%cSettings", position == 4 ? '>' : ' ');
-	oled_SetCursor(0, 0);
-	oled_WriteString(new_game, Font_7x10, White);
-	oled_SetCursor(0, 15);
-	oled_WriteString(load, Font_7x10, White);
-	oled_SetCursor(0, 30);
-	oled_WriteString(save, Font_7x10, White);
-	oled_SetCursor(0, 45);
-	oled_WriteString(settings, Font_7x10, White);
-	oled_UpdateScreen();
-}
-
-void write_symbol(uint8_t symbol) {
-	HAL_StatusTypeDef write_status;
-	do {
-		write_status = HAL_UART_Transmit(&huart6, &symbol, 1, 10000);
-	} while (write_status != HAL_OK);
-}
-
-void write_str(uint8_t* str) {
-	long length = strlen(str);
-	HAL_StatusTypeDef write_status;
-	do {
-		write_status = HAL_UART_Transmit(&huart6, str, length, 10000);
-	} while (write_status != HAL_OK);
-}
-
-void write_new_line() {
-	write_str("\n\r");
-}
-
-void write_line(uint8_t* str) {
-	write_str(str);
-	write_new_line();
-}
-
-enum key {
+enum KEY {
 	ONE = 110,
 	TWO = 94,
 	THREE = 62,
@@ -383,6 +112,49 @@ enum key {
 	TWELVE = 55,
 	NONE = 0
 };
+
+enum MENU_TYPE {
+	MAIN = 0,
+	SAVE = 1,
+	LOAD = 2,
+	SETTINGS = 4,
+	NEW_GAME = 8
+};
+
+
+print_menu(int position, MENU_TYPE menu_type) {
+	char new_game[50];
+	char load[50];
+	char save[50];
+	char settings[50];
+	oled_Fill(Black);
+
+	switch (menu_type){
+		case MAIN:
+			sprintf(new_game, "%cNew Game", position == 1 ? '>' : ' ');
+			sprintf(load, "%cLoad", position == 2 ? '>' : ' ');
+			sprintf(save, "%cSave", position == 3 ? '>' : ' ');
+			sprintf(settings, "%cSettings", position == 4 ? '>' : ' ');
+			oled_SetCursor(0, 0);
+			oled_WriteString(new_game, Font_7x10, White);
+			oled_SetCursor(0, 15);
+			oled_WriteString(load, Font_7x10, White);
+			oled_SetCursor(0, 30);
+			oled_WriteString(save, Font_7x10, White);
+			oled_SetCursor(0, 45);
+			oled_WriteString(settings, Font_7x10, White);
+			oled_UpdateScreen();
+			break;
+		case LOAD:
+		case SAVE:
+		case SETTINGS:
+			
+		default:
+			oled_SetCursor(0, 0);
+			oled_WriteString("Wrong menu type", Font_7x10, White);
+	}
+}
+
 
 uint8_t read_symbol(uint8_t row) {
 	uint8_t configuration_comman = 0x70;
@@ -405,7 +177,7 @@ uint8_t read_symbol(uint8_t row) {
 	return read_symbol;
 }
 
-enum key get_key() {
+enum KEY get_key() {
 
 
 	uint8_t symbol = read_symbol(0);
@@ -430,7 +202,6 @@ enum key get_key() {
 		return SIX;
 	}
 
-
 	symbol = read_symbol(2);
 	if (symbol == SEVEN) {
 		return SEVEN;
@@ -453,6 +224,39 @@ enum key get_key() {
 		return TWELVE;
 	}
 	return NONE;
+}
+
+enum MENU_TYPE get_menu_type(enum KEY current_key, int position){
+	if (current_key == TEN)
+		return MAIN;
+	switch (position){
+		case 1:
+			return NEW_GAME;
+		case 2:
+			return LOAD;
+		case 3:
+			return SAVE;
+		case 4:
+			return SETTINGS;
+		default:
+			return MAIN;
+	}
+	
+}
+
+int get_new_position(int position, enum MENU_TYPE menu_type, enum KEY current_key) {
+	switch (menu_type){
+		case MAIN:
+			if (current_key == TWO || current_key == EIGHT) {
+				if (current_key == TWO)
+					position +=2;
+				position = position % 4;
+				position++;
+			}
+			return position;
+		default:
+			return 0;
+	}
 }
 
 int main(void) {
@@ -494,10 +298,19 @@ int main(void) {
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
 	int position = 1;
+	enum MENU_TYPE menu_type = MAIN;
 	int prev_data = default_data;
 	while (1) {
-//		print_menu(position);
-//
+		 HAL_Delay(100);
+	 	 print_menu(position, menu_type);
+		 enum KEY current_key = get_key();
+		 if (current_key == NONE)
+			continue;
+		 if (current_key == FIVE || current_key == TEN) {
+			position = 1;
+			menu_type = get_menu_type();
+		 }
+		 position = get_new_position(position, menu_type, current_key);
 //		HAL_I2C_Mem_Read(&hi2c1, 0xe3, 0x00, I2C_MEMADD_SIZE_8BIT, &data,
 //				sizeof(uint8_t), 10);
 //		if (data != prev_data) {
@@ -511,17 +324,6 @@ int main(void) {
 //				position++;
 //			}
 //		}
-
-		 HAL_Delay(100);
-		 enum key current_key = get_key();
-		/*
-			  uint8_t symbol = read_symbol();
-			  	  	  if (symbol == 's' || symbol == 'c' || symbol == 't') {
-			  	  	  	  oled_Fill(Black);
-			  	  	  	  oled_SetCursor(56, 24);
-			  	  	  	  oled_WriteChar(symbol, Font_Geometric_16x16, White);
-			  	  	  	  oled_UpdateScreen();
-			  	  	  }*/
 	}
 
 	/* USER CODE END 3 */
